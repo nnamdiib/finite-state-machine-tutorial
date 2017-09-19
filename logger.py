0b64b55dc6e3908ac9670b1db9c3382496e7f5a9
Nnamdi Ibeanusi <nnamdiibns@gmail.com>
2017-09-19 18:27:32 +0100

    Add comments in logger and wrap func

--- logger.py
+++ logger.py
@@ -1,4 +1,10 @@
+# log is a decorator we will use to print out each state
+# transition from the initial to the final state.
+# FUnction decorators are another cool aspect of Python.
+import functools
+
 def log(func):
+  @functools.wraps(func) # Yup. We can still decorate functions within our decorator.
   def logged(*args):
     result = func(*args)
     print('I am in State %s,\
